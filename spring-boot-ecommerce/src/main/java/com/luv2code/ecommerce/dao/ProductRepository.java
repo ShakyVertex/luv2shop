aca99e120a3d706ad4adbce111e5f250f6a31f3c
SellaMomota <sellamomota@outlook.com>
2024-05-17 13:19:20 -0400

    Integrating Online Shop Template

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+// add CrossOrigin support for web browser
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -28,6 +28,9 @@ public class Product {
     @Column(name = "sku")
     private String sku;
 
+    @Column(name = "name")
+    private String name;
+
     @Column(name = "description")
     private String description;
 
