e3b81f8acb9e913cfc10221ae9f6d127ad4f2f84
SellaMomota <sellamomota@outlook.com>
2024-06-01 22:31:02 -0400

    Shopping Cart Items CRUD

--- angular-ecommerce/src/app/services/cart.service.ts
+++ angular-ecommerce/src/app/services/cart.service.ts
@@ -6,7 +6,7 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class CartService {
-
+  
   cartItems: CartItem[] = [];
 
   totalPrice: Subject<number> = new Subject<number>();
@@ -38,6 +38,27 @@ export class CartService {
 
     // compute cart total price and total quantity
     this.computeCartTotals();
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id );
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+      this.computeCartTotals;
+    }
   }
 
   computeCartTotals() {
