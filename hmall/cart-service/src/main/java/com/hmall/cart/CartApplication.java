0f87e8231c6bd4479765e16cb24af1de760d25d9
SellaMomota <sellamomota@outlook.com>
2024-07-06 22:05:13 -0400

    CartService

---
+++ hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -0,0 +1,22 @@
+package com.hmall.cart;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("com.hmall.cart.mapper")
+@SpringBootApplication
+@EnableFeignClients
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
